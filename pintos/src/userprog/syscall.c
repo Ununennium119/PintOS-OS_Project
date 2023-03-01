998add396c1be890dca8ffe21983a9c168526480
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2023-03-01 07:25:25 +0000

    add write to stdout

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -6,11 +6,15 @@
 
 static void syscall_handler (struct intr_frame *);
 
+int write_syscall (int fd, void *buffer, unsigned size);
+
 void
 syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
+
+
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
@@ -32,4 +36,29 @@ syscall_handler (struct intr_frame *f UNUSED)
       printf ("%s: exit(%d)\n", &thread_current ()->name, args[1]);
       thread_exit ();
     }
+  else if (args[0] == SYS_WRITE)
+    {
+      int fd = (int) args[1];
+      void *buffer = (void *) args[2];
+      unsigned size = (unsigned) args[3];
+
+      int result = write_syscall (fd, buffer, size);
+      f->eax = result;
+    }
+  else if (args[0] == SYS_PRACTICE)
+    {
+      f->eax = args[1] + 1;
+    }
+}
+
+int
+write_syscall (int fd, void *buffer, unsigned size)
+{
+  if (fd == STDOUT_FILENO)
+    {
+      putbuf(buffer, size);
+      return size;
+    }
+  
+  return -1;
 }
